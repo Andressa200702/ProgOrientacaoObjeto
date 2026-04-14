68a4e881f00ca976b4ad5d8fa4331413e4f9a4ca
andressa.sxavier2 <drexavier02@gmail.com>
2026-04-14 10:39:01 -0300

    POO 14/04

--- src/banco/classes/Main.java
+++ src/banco/classes/Main.java
@@ -1,4 +1,6 @@
+//Criar a classe CONTA que deve ter os atributos NÚMERO,
+// SALDO e TITULAR --> "Cliente". Titular deve ter necessariamente
+//NOME e CPF. A conta deve ser capaz de realizar SAQUE e DEPOSITO.
 package banco.classes;
-
 public class Main {
 }
